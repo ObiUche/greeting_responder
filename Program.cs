ce9c508d3c11a0e95ba069ba2e3a4bced2f93192
Obinna Uche <obinnauche01@outlook.com>
2025-05-23 21:43:34 +0100

    Intial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+- Greetings Responder
+- Take the users name 
+- Then respond with a greeting 
+
+*/
+
+string user_name = "";
+
+
+Console.WriteLine("Hello! What is your name?");
+
+user_name = Console.ReadLine();
+
+
