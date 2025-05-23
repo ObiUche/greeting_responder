2a14a7fbc3e095fda643b3745b3d88b1450a8270
Obinna Uche <obinnauche01@outlook.com>
2025-05-23 21:52:38 +0100

    took users response and assigned it to variable put error handling if null

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ string user_name = "";
 
 Console.WriteLine("Hello! What is your name?");
 
-user_name = Console.ReadLine();
+user_name = Console.ReadLine() ?? "";
+
+if (string.IsNullOrEmpty(user_name))
+{
+    Console.WriteLine("Please enter your name:");
+}
+else
+{
+    Console.WriteLine($"Hello {user_name}! Welcome to greetings responder!");
+}
+
 
 
